99ffa1a27c3f5ea65d098b01d31b6491930487c7
chaterinaolivia <230712313@students.uajy.ac.id>
2026-04-30 23:45:59 +0700

    controller model saveHotel

---
+++ backend/app/Http/Controllers/SaveHotelController.php
@@ -0,0 +1,61 @@
+<?php 
+
+namespace App\Http\Controllers; 
+
+use App\Models\SaveHotel; 
+use Illuminate\Http\Request; 
+
+class SaveHotelController extends Controller{
+    public function index(Request $request){
+        $savehotel = SaveHotel::with(
+            'hotel'
+        )->where('id_user', $request->id_user)
+         ->where('is_saved', true)
+         ->get(); 
+
+        return response()->json([
+            'message' => 'Data Save Hotel berhasil diambil', 
+            'data' => $savehotel, 
+        ], 200); 
+    }
+
+    public function store(Request $request){
+        $validated = $request->validate([
+            'id_user' => 'nullable|integer|exists:users,id_user', 
+            'id_hotel' => 'nullable|integer|exists:hotel,id_hotel', 
+        ]); 
+
+        $save = SaveHotel::where('id_user', $validated['id_user'])
+                    ->where('id_hotel', $validated['id_hotel'])
+                    ->first(); 
+
+        if($save){
+            $save->update([
+                'is_saved' => true
+            ]); 
+        }else{
+            $save = SaveHotel::create([
+                'id_user' => $validated['id_user'],
+                'id_hotel' => $validated['id_hotel'], 
+                'is_saved' => true
+            ]); 
+        }
+
+        return response()->json([
+            'message' => 'Data Save Hotel berhasil disimpan', 
+            'data' => $save, 
+        ], 201); 
+    }
+
+    public function update(Request $request, SaveHotel $saveHotel){
+        $saveHotel->update([
+            'is_saved' => !$saveHotel->is_saved
+        ]); 
+
+        return response()->json([
+            'message' => 'Status Save Hotel berhasil diubah',
+            'data' => $saveHotel
+        ], 200); 
+    }
+
+}
--- backend/app/Models/SaveHotel.php
+++ backend/app/Models/SaveHotel.php
@@ -1,1 +1,33 @@
-//chaterin
+<?php 
+
+namespace App\Models; 
+
+use Illuminate\Database\Eloquent\Model; 
+use Illuminate\Database\Eloquent\Factories\HasFactory; 
+use App\Models\User; 
+use App\Models\Hotel; 
+
+class SaveHotel extends Model{
+    use HasFactory; 
+
+    protected $table = 'save_hotel'; 
+
+    protected $primaryKey = 'id_savehotel';
+
+    protected $keyType = 'int';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'id_user',
+        'id_hotel',
+        'is_saved',
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class, 'id_user', 'id_user'); 
+    }
+    public function hotel(){
+        return $this->belongsTo(Hotel::class, 'id_hotel', 'id_hotel'); 
+    }
+}
